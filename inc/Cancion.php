8351be1d4ad7749fec11a696edf64c066aa0c321
JonatanTomillo <jonatan.tomillo.1993@gmail.com>
2017-02-13 13:43:30 +0100

    add Cancion class

---
+++ inc/Cancion.php
@@ -0,0 +1,24 @@
+<?php
+//clases para canciones
+require_once("conexion.php");
+class Cancion{
+	//ATRIBUTOS
+	private var $id;
+	private var $titulo;
+	private var $grupo;
+	private var $caratula;
+	private var $cancion;
+	private var $notas;
+	private var $duracion;
+
+	//CONSTRUCTOR
+
+	//METODOS
+
+
+	//ESTATICOS
+	public static function listarCanciones(){
+
+	}
+}
+?>
