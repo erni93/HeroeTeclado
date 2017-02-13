94019bfe4d6745757eef5df89523d872239cc185
pBLhawk <pabloruizortega94@gmail.com>
2017-02-13 13:15:22 +0100

    Clase usuario completa, falta probar

--- class/Usuario.php
+++ class/Usuario.php
@@ -6,16 +6,47 @@ class Usuario {
         $this->db = Conexion::dameInstancia();
     }
 
-    	public function insertar_usuario($nick,$password,$correo,$avatar){ //pendiente de prueba
+    public function insertar_usuario($nick,$password,$correo,$avatar){ //pendiente de prueba
         
-            $sentencia = $this->db->prepare("insert into usuarios (nick,password,correo,avatar) values (?,?,?,?)");
-            $sentencia->bindValue(1, $nick);
-            $sentencia->bindValue(2, $password);
-            $sentencia->bindValue(3, $correo);
-            $sentencia->bindValue(4, $avatar);
-           
-            $sentencia->execute();
+        $sentencia = $this->db->prepare("insert into usuarios (nick,password,correo,avatar) values (?,?,?,?)");
+        $password_md5=md5($password);
+        $sentencia->bindValue(1, $nick);
+        $sentencia->bindValue(2, $password_md5);
+        $sentencia->bindValue(3, $correo);
+        $sentencia->bindValue(4, $avatar);
+       
+        $sentencia->execute();
+        
+    }
 
-        
+    public function login_usuario($email,$password){ //pendiente de prueba
+
+    	$success=false;
+
+    	$sql="select * from usuarios where correo = ? and password = ?";
+    	$consulta = $this->db->prepare($sql);
+
+    	$password_md5 = md5($password);
+        $consulta->bindValue(1, $email);
+        $consulta->bindValue(2, $password_md5);
+        $consulta->execute();
+
+        //Si existe en la BBDD
+        if($consulta->numrows()==1){
+        	$fila = $consulta->fetch();
+            $_SESSION['id'] = $fila['id'];
+            $_SESSION['correo'] = $fila['correo'];
+            $_SESSION['nick'] = $fila['nick'];
+            $_SESSION['password'] = $fila['password'];
+            $_SESSION['rango'] = $fila['rango'];
+            $_SESSION['avatar'] = $fila['avatar'];
+            $success=true;
+        }
+
+        return $success;
+    }
+
+    public function __clone() {
+       return "La clonacion de este objeto no esta permitida";
     }
 }
