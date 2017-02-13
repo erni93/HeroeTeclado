1ce7d1184a76a32b61ced37c7d1019c5bb38ef35
pBLhawk <pabloruizortega94@gmail.com>
2017-02-13 12:12:54 +0100

    inclusion html

---
+++ inc/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>¡Regístrate!</title>
+		<link rel="stylesheet" href="./style/main.css" type="text/css" />
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<script src="js/bootstrap.min.js"></script>
+		<script type="js/jquery-3.1.1.slim.min.js"></script>
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li><a href="#anclaPpal">Juego</a></li>
+					<li><a href="#anclaNovedad">Novedades</a></li>
+					<li><a href="#anclaPunt">Puntuaciones</a></li>
+					<li><a href="#anclaCancion">Canciones</a></li>
+					<li><a href="/inc/login.php">Cuenta</a></li>
+				</ul>
+			</nav>
+		</header>
+		<div id="contenedor">
+			<form>
+				<fieldset>
+					
+				</fieldset>
+			</form>
+		</div>
+		<footer>
+			<h2>Página desarrollada por los estudiantes de DAW:</h2>
+			<ul>
+				<li>David Parro</li>
+				<li>Ernesto del Valle</li>
+				<li>Jonatan Tomillo</li>
+				<li>Renzo Roca</li>
+				<li>Pablo Ruiz</li>
+			</ul>
+		</footer>
+	</body>
+</html>
