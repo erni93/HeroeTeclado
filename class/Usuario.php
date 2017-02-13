6bb05a6263f46639e75edfed3c1a08046c281a8b
pBLhawk <pabloruizortega94@gmail.com>
2017-02-13 12:57:13 +0100

    funcion insertar usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -5,4 +5,17 @@ class Usuario {
     public function __construct() {
         $this->db = Conexion::dameInstancia();
     }
+
+    	public function insertar_usuario($nick,$password,$correo,$avatar){ //pendiente de prueba
+        
+            $sentencia = $this->db->prepare("insert into usuarios (nick,password,correo,avatar) values (?,?,?,?)");
+            $sentencia->bindValue(1, $nick);
+            $sentencia->bindValue(2, $password);
+            $sentencia->bindValue(3, $correo);
+            $sentencia->bindValue(4, $avatar);
+           
+            $sentencia->execute();
+
+        
+    }
 }
