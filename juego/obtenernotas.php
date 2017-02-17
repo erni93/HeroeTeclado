1e2aa86aed9a8b9ccdbc8a8430382e6156ba6ff4
Ernesto <erni.delvalle@outlook.es>
2017-02-17 11:40:39 +0100

    Optimizacion get

--- juego/obtenernotas.php
+++ juego/obtenernotas.php
@@ -1,11 +1,6 @@
 <?php
 	require("crearcancion.php");
-	if (isset($_GET["action"]) && $_GET["action"] == "notas") {
-		// total notas 1064
-		//recuperar el array de notas de la sesion
-		$notas = crearNotas();
-		echo json_encode($notas);
-	}
-
-
+	$notas = crearNotas();
+	echo json_encode($notas);
+	
  ?>
