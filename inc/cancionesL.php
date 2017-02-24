1d23a5f7bd2e269cb20d5cddcb5141c7ea724b8f
JonatanTomillo <jonatan.tomillo.1993@gmail.com>
2017-02-24 13:19:22 +0100

    cambia cancion

---
+++ inc/cancionesL.php
@@ -0,0 +1,28 @@
+<?php
+  require_once("../class/Cancion.php");
+  require_once("../class/Conexion.php");
+  require_once("funciones.inc.php");
+  iniciarSesion();
+
+  if(isset($_POST['c'])){
+    switch ($_POST['c']) {
+      case 'listar':
+        listarCanciones();
+        break;
+      case 'cambiar':
+        $_SESSION['cancion']=$_POST['cancion'];
+        echo $_SESSION['cancion'];
+        break;
+      default:
+        # code...
+        break;
+    }
+  }
+
+function listarCanciones(){
+  $canciones= new Cancion;
+  $lista=$canciones->verCanciones();
+  echo json_encode($lista);
+}
+
+ ?>
