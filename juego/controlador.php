17e55b99461a9129ab149d5fd4d1eaeb7489aa31
Ernesto del valle <erni.delvalle@outlook.es>
2017-02-25 20:55:32 +0000

    Canciones añadidas y rutas corregidas

--- juego/controlador.php
+++ juego/controlador.php
@@ -4,23 +4,25 @@
 	iniciarSesion();
 	//Obtener cancion seleccionada
 	//$_SESSION["cancion"] = "30";
-	if (isset($_SESSION['id']) && isset($_SESSION["cancion"])){
-		if (isset($_REQUEST["action"])){
-			$action = $_REQUEST["action"];
-			if ($action == "actualizar"){
-				if (isset($_REQUEST["puntuacion"])){
-					$id_cancion = $_SESSION["cancion"];
-					$id_usuario = $_SESSION['id'];
-					$puntuacion = intval($_REQUEST["puntuacion"]);
-					//Verificar el tiempo de inicio del juego y el final para evitar falsas puntuaciones
-					$instancia=Conexion::dameInstancia();
-					$db=$instancia->conexion();
-					$sql="INSERT INTO puntuaciones (id_cancion, id_usuario, puntuacion, fecha) VALUES ({$id_cancion}, {$id_usuario}, {$puntuacion}, datetime('now','localtime'))";
-					$consulta=$db->query($sql);
-					$consulta->finalize();
-				}
-			}else if ($action == "cancion"){
+	if (isset($_REQUEST["action"])){
+		$action = $_REQUEST["action"];
+		if ($action == "actualizar"){
+			if (isset($_REQUEST["puntuacion"])){
 				$id_cancion = $_SESSION["cancion"];
+				$id_usuario = $_SESSION['id'];
+				$puntuacion = intval($_REQUEST["puntuacion"]);
+				//Verificar el tiempo de inicio del juego y el final para evitar falsas puntuaciones
+				$instancia=Conexion::dameInstancia();
+				$db=$instancia->conexion();
+				$sql="INSERT INTO puntuaciones (id_cancion, id_usuario, puntuacion, fecha) VALUES ({$id_cancion}, {$id_usuario}, {$puntuacion}, datetime('now','localtime'))";
+				$consulta=$db->query($sql);
+				$consulta->finalize();
+			}
+		}else if ($action == "cancion"){
+				$id_cancion = $_SESSION["cancion"];
+				if (empty($id_cancion)){
+					$id_cancion = "canciones/So-Payaso_Extremoduro";
+				}
 				$instancia=Conexion::dameInstancia();
 				$db=$instancia->conexion();
 				$sql="SELECT ruta FROM canciones WHERE id={$id_cancion}";
@@ -29,11 +31,7 @@
 				$ruta = $resultado["ruta"];
 				$consulta->finalize();
 				echo $ruta;
-			}
 		}
-
-	}else{
-		echo "canciones/So-Payaso_Extremoduro";
 	}
 
  ?>
